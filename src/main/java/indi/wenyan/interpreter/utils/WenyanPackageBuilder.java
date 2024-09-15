c8c69fb09cbfeaf9c8d4cf8d0c6e490ae0a08dfc
gyxx-xc <xdll_xc@outlook.com>
2024-09-15 16:01:02 +0800

    add more function import method

--- src/main/java/indi/wenyan/interpreter/utils/WenyanPackageBuilder.java
+++ src/main/java/indi/wenyan/interpreter/utils/WenyanPackageBuilder.java
@@ -1,5 +1,7 @@
 package indi.wenyan.interpreter.utils;
 
+import java.util.function.Function;
+
 public class WenyanPackageBuilder {
     private final WenyanFunctionEnvironment environment = new WenyanFunctionEnvironment();
 
@@ -16,25 +18,40 @@ public class WenyanPackageBuilder {
         return environment;
     }
 
-    public WenyanPackageBuilder function(String name, JavacallHandler.WenyanFunction function) {
-        WenyanFunctionEnvironment.FunctionSign sign = new WenyanFunctionEnvironment.FunctionSign(name, new WenyanValue.Type[0]);
-        environment.setVariable(name, new WenyanValue(WenyanValue.Type.FUNCTION, sign, true));
-        environment.setFunction(sign, new JavacallHandler(function));
+    public WenyanPackageBuilder constant(String name, WenyanValue.Type type, Object value) {
+        environment.setVariable(name, new WenyanValue(type, value, true));
         return this;
+    }
+
+    public WenyanPackageBuilder function(String name, Function<Object[], Object> function,
+                                         WenyanValue.Type returnType, WenyanValue.Type[] argTypes) {
+        return function(name, args -> {
+            Object[] newArgs = new Object[args.length];
+            for (int i = 0; i < argTypes.length; i++)
+                newArgs[i] = args[i].casting(argTypes[i]).getValue();
+            return new WenyanValue(returnType, function.apply(newArgs), true);
+        }, new WenyanValue.Type[0]);
+    }
+
+    public WenyanPackageBuilder function(String name, Function<Object[], Object> function, WenyanValue.Type valueType) {
+        return function(name, args -> {
+            Object[] newArgs = new Object[args.length];
+            for (int i = 0; i < args.length; i++)
+                newArgs[i] = args[i].casting(valueType).getValue();
+            return new WenyanValue(valueType, function.apply(newArgs), true);
+        }, new WenyanValue.Type[0]);
+    }
+
+    public WenyanPackageBuilder function(String name, JavacallHandler.WenyanFunction function) {
+        return function(name, function, new WenyanValue.Type[0]);
     }
 
     public WenyanPackageBuilder function(String name, JavacallHandler.WenyanFunction function, WenyanValue.Type[] argTypes) {
-        WenyanFunctionEnvironment.FunctionSign sign = new WenyanFunctionEnvironment.FunctionSign(name, argTypes);
-        environment.setVariable(name, new WenyanValue(WenyanValue.Type.FUNCTION, sign, true));
-        environment.setFunction(sign, new JavacallHandler(function));
-        return this;
+        return function(name, new JavacallHandler(function), argTypes);
     }
 
     public WenyanPackageBuilder function(String name, JavacallHandler javacall) {
-        WenyanFunctionEnvironment.FunctionSign sign = new WenyanFunctionEnvironment.FunctionSign(name, new WenyanValue.Type[0]);
-        environment.setVariable(name, new WenyanValue(WenyanValue.Type.FUNCTION, sign, true));
-        environment.setFunction(sign, javacall);
-        return this;
+        return function(name, javacall, new WenyanValue.Type[0]);
     }
 
     public WenyanPackageBuilder function(String name, JavacallHandler javacall, WenyanValue.Type[] argTypes) {
@@ -43,5 +60,4 @@ public class WenyanPackageBuilder {
         environment.setFunction(sign, javacall);
         return this;
     }
-
 }
