bfd0036f569a2d800049c57237b02372e2c98e35
gyxx-xc <xdll_xc@outlook.com>
2024-09-18 01:14:30 +0800

    refactor

--- src/main/java/indi/wenyan/entity/BulletEntity.java
+++ src/main/java/indi/wenyan/entity/BulletEntity.java
@@ -3,6 +3,7 @@ package indi.wenyan.entity;
 import indi.wenyan.setup.Registration;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
--- src/main/java/indi/wenyan/setup/ClientSetup.java
+++ src/main/java/indi/wenyan/setup/ClientSetup.java
@@ -7,8 +7,12 @@ import indi.wenyan.entity.HandlerEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.references.Blocks;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -46,4 +50,5 @@ public class ClientSetup {
             }
         });
     }
+
 }
