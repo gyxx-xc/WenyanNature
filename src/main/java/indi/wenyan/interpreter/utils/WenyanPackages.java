f15b168fc85d47f8f2131235508daf09a2626a01
gyxx-xc <xdll_xc@outlook.com>
2024-12-29 02:02:40 +0800

    ban some function for teacon

--- src/main/java/indi/wenyan/interpreter/utils/WenyanPackages.java
+++ src/main/java/indi/wenyan/interpreter/utils/WenyanPackages.java
@@ -151,11 +151,11 @@ public class WenyanPackages {
                 .environment(WENYAN_BASIC_PACKAGES)
                 .function("書", new OutputHandler(holder))
                 .function("「觸」", new TouchHandler(holder.level(), pos), TouchHandler.ARGS_TYPE)
-                .function("「放置」", new BlockPlaceHandler(holder,
-                        (BlockItem) Items.ACACIA_LOG.asItem()
-                        ,pos, block))
-                .function("「移」", new BlockMoveHandler(holder, pos, block), BlockMoveHandler.ARGS_TYPE)
-                .function("「放」", new CommunicateHandler(pos, block, holder.level()), CommunicateHandler.ARG_TYPES)
+                // .function("「放置」", new BlockPlaceHandler(holder,
+                //         (BlockItem) Items.ACACIA_LOG.asItem()
+                //         ,pos, block))
+                // .function("「移」", new BlockMoveHandler(holder, pos, block), BlockMoveHandler.ARGS_TYPE)
+                // .function("「放」", new CommunicateHandler(pos, block, holder.level()), CommunicateHandler.ARG_TYPES)
                 .function("「紅石量」", new RedstoneSignalHandler(thread, runner))
                 .function("「己於上」", new SelfPositionBlockHandler(holder, runner, Direction.UP))
                 .function("「己於下」", new SelfPositionBlockHandler(holder, runner, Direction.DOWN))
