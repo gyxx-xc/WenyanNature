aafde9a6a96a350be61af84109ce77d3be913de0
loliking-01 <2403254959@qq.com>
2025-06-16 16:00:08 +0800

    忘记改这个了

--- src/main/java/indi/wenyan/content/handler/feature_additions/string_util_handler/StringUtilSubStringStartAndEndHandler.java
+++ src/main/java/indi/wenyan/content/handler/feature_additions/string_util_handler/StringUtilSubStringStartAndEndHandler.java
@@ -1,10 +1,13 @@
 package indi.wenyan.content.handler.feature_additions.string_util_handler;
 
 import indi.wenyan.content.handler.JavacallHandler;
+import indi.wenyan.interpreter.structure.JavacallContext;
 import indi.wenyan.interpreter.structure.WenyanException;
 import indi.wenyan.interpreter.structure.WenyanNativeValue;
 import indi.wenyan.interpreter.structure.WenyanType;
 import indi.wenyan.interpreter.utils.JavacallHandlers;
+
+import java.util.List;
 
 /**
  * @author I_am_a_lolikong
@@ -16,12 +19,13 @@ import indi.wenyan.interpreter.utils.JavacallHandlers;
 public class StringUtilSubStringStartAndEndHandler implements JavacallHandler {
     public static final WenyanType[] ARGS_TYPE =
             {WenyanType.STRING, WenyanType.INT, WenyanType.INT};
+
     @Override
-    public WenyanNativeValue handle(WenyanNativeValue[] wenyan_args) throws WenyanException.WenyanThrowException {
-        Object[] args = JavacallHandlers.getArgs(wenyan_args, ARGS_TYPE);
-        String original=args[0].toString();
-        int start=Integer.parseInt(args[1].toString());
-        int end=Integer.parseInt(args[2].toString());
+    public WenyanNativeValue handle(JavacallContext context) throws WenyanException.WenyanThrowException {
+        List<Object> args = JavacallHandlers.getArgs(context.args(), ARGS_TYPE);
+        String original= args.get(0).toString();
+        int start=Integer.parseInt(args.get(1).toString());
+        int end=Integer.parseInt(args.get(2).toString());
         if (start>original.length()){
             throw new WenyanException("謬：始数不可超文长");
         }else if (start<0){
