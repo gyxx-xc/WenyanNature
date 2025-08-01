13cb3731143fea67b2f98a9fca2bc10f56f562c3
gyxx-xc <xdll_xc@outlook.com>
2025-08-02 04:42:51 +0800

    feat: save package name

--- src/main/java/indi/wenyan/content/block/additional_module/AbstractAdditionalModuleEntity.java
+++ src/main/java/indi/wenyan/content/block/additional_module/AbstractAdditionalModuleEntity.java
@@ -1,31 +1,40 @@
 package indi.wenyan.content.block.additional_module;
 
+import indi.wenyan.content.block.DataBlockEntity;
 import indi.wenyan.content.handler.IExecCallHandler;
 import indi.wenyan.interpreter.structure.values.WenyanPackage;
 import indi.wenyan.interpreter.utils.IWenyanDevice;
 import lombok.Getter;
-import lombok.Setter;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Objects;
 import java.util.Optional;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public abstract class AbstractAdditionalModuleEntity extends BlockEntity implements IWenyanDevice {
+public abstract class AbstractAdditionalModuleEntity extends DataBlockEntity implements IWenyanDevice {
     @Getter
     private final ExecQueue execQueue = new ExecQueue();
 
-    @Setter
+    @Nullable
     private String packageName;
 
-    public AbstractAdditionalModuleEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
+    public void setPackageName(@Nullable String packageName) {
+        this.packageName = packageName;
+        setChanged();
+    }
+
+    protected AbstractAdditionalModuleEntity(
+            BlockEntityType<? extends AbstractAdditionalModuleEntity> type,
+            BlockPos pos, BlockState blockState) {
         super(type, pos, blockState);
     }
 
@@ -42,6 +51,18 @@ public abstract class AbstractAdditionalModuleEntity extends BlockEntity impleme
     public abstract String getBasePackageName();
 
     public abstract WenyanPackage getExecPackage();
+
+    @Override
+    protected void saveData(CompoundTag tag, HolderLookup.Provider registries) {
+        if (packageName != null)
+            tag.putString("packageName", packageName);
+    }
+
+    @Override
+    protected void loadData(CompoundTag tag, HolderLookup.Provider registries) {
+        if (tag.contains("packageName"))
+            packageName = tag.getString("packageName");
+    }
 
     abstract class ThisCallHandler implements IExecCallHandler {
         @Override
