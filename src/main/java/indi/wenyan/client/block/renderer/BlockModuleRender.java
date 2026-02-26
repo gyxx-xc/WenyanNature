c5b8d8594f991ad8f2d943f2dbf47a655ed94533
gyxx-xc <xdll_xc@outlook.com>
2026-02-26 13:52:57 +0800

    refactor(renderer): update block module rendering implementation

--- src/main/java/indi/wenyan/client/block/renderer/BlockModuleRender.java
+++ src/main/java/indi/wenyan/client/block/renderer/BlockModuleRender.java
@@ -1,18 +1,17 @@
 package indi.wenyan.client.block.renderer;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.DepthTestFunction;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import indi.wenyan.WenyanProgramming;
 import indi.wenyan.content.block.additional_module.paper.BlockModuleEntity;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
-import net.minecraft.client.renderer.rendertype.RenderSetup;
-import net.minecraft.client.renderer.rendertype.RenderType;
-import net.minecraft.client.renderer.rendertype.RenderTypes;
+import net.minecraft.client.renderer.rendertype.*;
 import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.Identifier;
@@ -25,6 +24,8 @@ import org.jspecify.annotations.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.awt.*;
 
+import static net.minecraft.client.renderer.RenderPipelines.LINES_SNIPPET;
+
 @ParametersAreNonnullByDefault
 public class BlockModuleRender implements BlockEntityRenderer<BlockModuleEntity, BlockModuleRender.BlockModuleRenderState> {
     public static final Identifier AABB_WALL = Identifier.fromNamespaceAndPath(WenyanProgramming.MODID, "textures/block/wall.png");
@@ -33,28 +34,21 @@ public class BlockModuleRender implements BlockEntityRenderer<BlockModuleEntity,
     }
 
     public static final RenderType FRONT_LINES = RenderType.create("lines",
-            RenderSetup.builder(RenderPipelines.LINES)
-                    .setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
-                    .createRenderSetup()
-    );
-//            DefaultVertexFormat.POSITION_COLOR_NORMAL, VertexFormat.Mode.LINES, 1536,
-//            RenderType.CompositeState.builder()
-//                    .setShaderState(RENDERTYPE_LINES_SHADER)
-//                    .setLineState(new LineStateShard(OptionalDouble.empty()))
-//                    .setLayeringState(VIEW_OFFSET_Z_LAYERING)
-//                    .setTransparencyState(TRANSLUCENT_TRANSPARENCY)
-//                    .setOutputState(ITEM_ENTITY_TARGET)
-//                    .setWriteMaskState(COLOR_DEPTH_WRITE)
-//                    .setCullState(NO_CULL)
-//                    .setDepthTestState(NO_DEPTH_TEST)
-//                    .createCompositeState(false));
+            RenderSetup.builder(RenderPipeline
+                            .builder(LINES_SNIPPET)
+                            .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
+                            .withLocation("pipeline/lines").build())
+                    .setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
+                    .setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
+                    .createRenderSetup());
 
     @Override
     public void extractRenderState(BlockModuleEntity blockEntity, BlockModuleRenderState state, float partialTicks, Vec3 cameraPosition, ModelFeatureRenderer.@Nullable CrumblingOverlay breakProgress) {
         BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTicks, cameraPosition, breakProgress);
         if (blockEntity.getContinueCount() > 0) {
             state.shouldRender = true;
-            state.aabb = new AABB(blockEntity.getRenderRange().start(), blockEntity.getRenderRange().end());
+            var aabb = new AABB(blockEntity.getRenderRange().start(), blockEntity.getRenderRange().end());
+            state.aabb = aabb.inflate(0.1); // avoid z confliect
             if (blockEntity.getRenderRange().found()) {
                 state.color = new Color(0xCCFFCC);
             } else {
@@ -74,9 +68,9 @@ public class BlockModuleRender implements BlockEntityRenderer<BlockModuleEntity,
                 ((pose, vertexConsumer) -> renderFaces(vertexConsumer, pose,
                         blockModuleRenderState.aabb, blockModuleRenderState.color)));
 
-//        vertexconsumer = bufferSource.getBuffer(FRONT_LINES);
-//        LevelRenderer(poseStack, vertexconsumer,
-//                aabb, 0.9f, 0.9f, 0.9f, 0.9f);
+        submitNodeCollector.submitCustomGeometry(poseStack, FRONT_LINES,
+                ((pose, vertexConsumer) ->
+                        renderEdges(vertexConsumer, pose, blockModuleRenderState.aabb)));
         poseStack.popPose();
     }
 
@@ -140,6 +134,50 @@ public class BlockModuleRender implements BlockEntityRenderer<BlockModuleEntity,
                 color, alpha, (float) (aabb.maxZ - aabb.minZ), 0, combinedLight);
     }
 
+    private static void renderEdges(VertexConsumer vertexconsumer, PoseStack.Pose pose, AABB aabb) {
+        int combinedLight = LightCoordsUtil.FULL_BRIGHT;
+        int alpha = 255;
+        Color color = Color.WHITE;
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.minY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.minX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.minZ, color, alpha, combinedLight, 1.0f);
+        lineVertex(vertexconsumer, pose, (float) aabb.maxX, (float) aabb.maxY, (float) aabb.maxZ, color, alpha, combinedLight, 1.0f);
+    }
+
     private static void vertex(
             VertexConsumer consumer, PoseStack.Pose pose,
             float x, float y, float z,
@@ -150,6 +188,16 @@ public class BlockModuleRender implements BlockEntityRenderer<BlockModuleEntity,
                 .setUv(u, v)
                 .setOverlay(OverlayTexture.NO_OVERLAY).setLight(packedLight)
                 .setNormal(pose, 0.0F, 1.0F, 0.0F);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static void lineVertex(
+            VertexConsumer consumer, PoseStack.Pose pose,
+            float x, float y, float z,
+            Color color, int alpha, int packedLight,
+            float lineWidth) {
+        vertex(consumer, pose, x, y, z, color, alpha, 0, 0, packedLight);
+        consumer.setLineWidth(lineWidth);
     }
 
     @Override
