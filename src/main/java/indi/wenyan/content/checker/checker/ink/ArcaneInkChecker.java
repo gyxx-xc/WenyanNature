f91e7999cd7309017c9624e09f463d521449943f
Rokidna A <qq617462323@outlook.com>
2026-03-24 00:54:29 +0800

    feat: ArcaneInkChecker pass

--- src/main/java/indi/wenyan/content/checker/checker/ink/ArcaneInkChecker.java
+++ src/main/java/indi/wenyan/content/checker/checker/ink/ArcaneInkChecker.java
@@ -1,9 +1,9 @@
 package indi.wenyan.content.checker.checker.ink;
 
 import indi.wenyan.content.checker.CraftingAnswerChecker;
-import indi.wenyan.content.checker.IAnsweringChecker;
-import indi.wenyan.judou.structure.WenyanException;
-import indi.wenyan.judou.structure.values.IWenyanValue;
+import java.util.Scanner;
+import java.math.BigInteger;
+
 import indi.wenyan.judou.utils.WenyanValues;
 import net.minecraft.util.RandomSource;
 
@@ -15,73 +15,36 @@ public class ArcaneInkChecker extends CraftingAnswerChecker {
     @Override
     public void init() {
         super.init();
-        String expr = generateExpression();
-        setVariable(0, WenyanValues.of(expr));
+        int n = random.nextInt(199) + 1;
+        setVariable(0,WenyanValues.of(n));
 
-        boolean isMatching = checkMatching(expr);
-        // FIXME: may change to const
-        ans = WenyanValues.of(isMatching ? "YES" : "NO");
-    }
+        int[] a = new int[2000];
+        a[1] = 1;
+        int len = 1;
 
-    private String generateExpression() {
-        StringBuilder sb = new StringBuilder();
-        int length = random.nextInt(100) + 10;
-        int leftParens = 0;
-        int openParens = 0;
-
-        boolean wantValid = random.nextBoolean();
-
-        for (int i = 0; i < length; i++) {
-            int type = random.nextInt(4);
-            if (type == 0 && leftParens < 19 && (wantValid || random.nextBoolean())) {
-                sb.append('(');
-                leftParens++;
-                openParens++;
-            } else if (type == 1 && openParens > 0 && (wantValid || random.nextBoolean())) {
-                sb.append(')');
-                openParens--;
-            } else if (type == 2) {
-                sb.append((char) ('a' + random.nextInt(26)));
-            } else {
-                char[] ops = { '+', '-', '*', '/' };
-                sb.append(ops[random.nextInt(4)]);
+        for (int i = 0; i < n + 1; i++) {
+            int carry = 0;
+            for (int j = 1; j <= len; j++) {
+                a[j] = a[j] * 2 + carry;
+                carry = a[j] / 10;
+                a[j] %= 10;
+            }
+            while (carry > 0) {
+                len++;
+                a[len] = carry % 10;
+                carry /= 10;
             }
         }
 
-        if (wantValid) {
-            while (openParens > 0) {
-                sb.append(')');
-                openParens--;
-            }
-        } else {
-            if (random.nextBoolean()) {
-                if (leftParens < 19) {
-                    sb.append('(');
-                } else {
-                    sb.append(')');
-                }
-            } else {
-                sb.append(')');
-            }
+        // 个位减 2
+        a[1] -= 2;
+
+        // 倒序拼接输出结果
+        StringBuilder output = new StringBuilder();
+        for (int i = len; i > 0; i--) {
+            output.append(a[i]);
         }
 
-        sb.append('@');
-        return sb.toString();
-    }
-
-    private boolean checkMatching(String a) {
-        int balance = 0;
-        for (int i = 0; i < a.length(); i++) {
-            char c = a.charAt(i);
-            if (c == '(') {
-                balance++;
-            } else if (c == ')') {
-                balance--;
-                if (balance < 0) {
-                    return false;
-                }
-            }
-        }
-        return balance == 0;
+        ans = WenyanValues.of(output.toString());
     }
 }
