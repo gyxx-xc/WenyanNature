12ac4f0d9c4970027fe046ba37b5293243671e0a
Rokidna A <qq617462323@outlook.com>
2026-03-24 00:54:29 +0800

    feat: ArcaneInkChecker pass

--- src/test/java/indi/wenyan/content/checker/checker/ink/ArcaneInkCheckerTest.java
+++ src/test/java/indi/wenyan/content/checker/checker/ink/ArcaneInkCheckerTest.java
@@ -1,5 +1,44 @@
 package indi.wenyan.content.checker.checker.ink;
 
-class ArcaneInkCheckerTest {
+import indi.wenyan.content.checker.IAnsweringChecker;
+import indi.wenyan.content.checker.checker.test_utils.MockRandomSource;
+import indi.wenyan.judou.structure.WenyanException;
+import indi.wenyan.judou.utils.WenyanValues;
+import net.minecraft.util.RandomSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+class ArcaneInkCheckerTest {
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "2, 6",
+    })
+    void testCorrectAnswer(int n, String expected) throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addSeq(n - 1)
+                .build();
+        ArcaneInkChecker checker = new ArcaneInkChecker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(expected));
+        Assertions.assertEquals(IAnsweringChecker.ResultStatus.ANSWER_CORRECT, checker.getResult());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 3",
+            "2, 7"
+    })
+    void testWrongAnswer(int n, String wrongAnswer) throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addSeq(n - 1)
+                .build();
+        ArcaneInkChecker checker = new ArcaneInkChecker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(wrongAnswer));
+        Assertions.assertEquals(IAnsweringChecker.ResultStatus.WRONG_ANSWER, checker.getResult());
+    }
 }
