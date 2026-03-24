0f24b1c0d56bf3ef7821043ef6185163ab6f22e5
gyxx-xc <xdll_xc@outlook.com>
2026-03-24 20:33:03 +0800

    feat: implement HandRunner6Checker logic and tests

--- src/main/java/indi/wenyan/content/checker/checker/handrunner/HandRunner6Checker.java
+++ src/main/java/indi/wenyan/content/checker/checker/handrunner/HandRunner6Checker.java
@@ -1,18 +1,42 @@
 package indi.wenyan.content.checker.checker.handrunner;
 
-import indi.wenyan.content.checker.ValueAnswerChecker;
+import com.google.common.math.IntMath;
+import indi.wenyan.content.checker.CraftingAnswerChecker;
+import indi.wenyan.judou.structure.WenyanException;
+import indi.wenyan.judou.structure.values.IWenyanValue;
+import indi.wenyan.judou.structure.values.primitive.WenyanInteger;
 import indi.wenyan.judou.utils.WenyanValues;
 import net.minecraft.util.RandomSource;
 
-public class HandRunner6Checker extends ValueAnswerChecker {
+public class HandRunner6Checker extends CraftingAnswerChecker {
     public HandRunner6Checker(RandomSource random) {
         super(random);
     }
 
+    private int a, m;
+    // a * x === 1 (mod m)
+
     @Override
     public void init() {
         super.init();
-        // TODO: Implement specific initialization logic and answer generation
-        ans = WenyanValues.of(0);
+        a = random.nextInt(2, Integer.MAX_VALUE);
+        m = random.nextInt(2, Integer.MAX_VALUE);
+
+        setVariable(0, WenyanValues.of(a));
+        setVariable(1, WenyanValues.of(m));
+    }
+
+    @Override
+    public void accept(IWenyanValue value) throws WenyanException {
+        int i = value.as(WenyanInteger.TYPE).value();
+        if (IntMath.gcd(a, m) != 1) {
+            setResult(i == -1 ? ResultStatus.ANSWER_CORRECT : ResultStatus.WRONG_ANSWER);
+            return;
+        }
+        if (((long) i * a) % m == 1) {
+            setResult(ResultStatus.ANSWER_CORRECT);
+        } else {
+            setResult(ResultStatus.WRONG_ANSWER);
+        }
     }
 }
--- src/test/java/indi/wenyan/content/checker/checker/handrunner/HandRunner6CheckerTest.java
+++ src/test/java/indi/wenyan/content/checker/checker/handrunner/HandRunner6CheckerTest.java
@@ -1,5 +1,107 @@
 package indi.wenyan.content.checker.checker.handrunner;
 
+import indi.wenyan.content.checker.IAnsweringChecker;
+import indi.wenyan.content.checker.checker.test_utils.MockRandomSource;
+import indi.wenyan.judou.structure.WenyanException;
+import indi.wenyan.judou.utils.WenyanValues;
+import net.minecraft.util.RandomSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class HandRunner6CheckerTest {
 
+    @ParameterizedTest
+    @CsvSource({
+            "3, 7, 5",
+            "5, 11, 9",
+            "2, 5, 3",
+            "7, 4, 3",
+            "9, 2, 1",
+            "2, 4, -1",
+            "6, 9, -1",
+            "4, 6, -1",
+            "10, 15, -1",
+    })
+    void testCorrectAnswer(int a, int m, int expectedX) throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addLong(a)
+                .addLong(m)
+                .build();
+
+        IAnsweringChecker checker = new HandRunner6Checker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(expectedX));
+        Assertions.assertEquals(IAnsweringChecker.ResultStatus.ANSWER_CORRECT, checker.getResult());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 7, 4",     // not 5
+            "5, 11, 8",    // not 9
+            "2, 5, 2",     // not 3
+            "2, 4, 2",     // gcd≠1, not -1
+            "6, 9, 3",     // gcd≠1, not -1
+            "7, 4, 2",     // not 3
+            "9, 2, 2"      // not 1
+    })
+    void testWrongAnswer(int a, int m, int wrongX) throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addLong(a)
+                .addLong(m)
+                .build();
+
+        IAnsweringChecker checker = new HandRunner6Checker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(wrongX));
+        assertEquals(IAnsweringChecker.ResultStatus.WRONG_ANSWER, checker.getResult());
+    }
+
+
+    @Test
+    void testZeroAnswer() throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addLong(3)
+                .addLong(7)
+                .build();
+
+        IAnsweringChecker checker = new HandRunner6Checker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(0));
+        assertEquals(IAnsweringChecker.ResultStatus.WRONG_ANSWER, checker.getResult());
+    }
+
+    @Test
+    void testNegativeAnswer() throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addLong(3)
+                .addLong(7)
+                .build();
+
+        IAnsweringChecker checker = new HandRunner6Checker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(-2));
+        assertEquals(IAnsweringChecker.ResultStatus.WRONG_ANSWER, checker.getResult());
+    }
+
+    @Test
+    void testOffByOne() throws WenyanException {
+        RandomSource random = MockRandomSource.InputBuilder.create()
+                .addLong(3)
+                .addLong(7)
+                .build();
+
+        IAnsweringChecker checker = new HandRunner6Checker(random);
+        checker.init();
+
+        checker.accept(WenyanValues.of(4)); // 5-1
+        assertEquals(IAnsweringChecker.ResultStatus.WRONG_ANSWER, checker.getResult());
+    }
 }
