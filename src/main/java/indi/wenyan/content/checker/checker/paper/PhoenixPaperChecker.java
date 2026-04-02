f22b75ca7def19919db3874e866cd717e5279dd7
Rokidna IV <rokidna@outlook.com>
2026-04-03 02:01:28 +0800

    feat: PhoenixPaperChecker pass

--- src/main/java/indi/wenyan/content/checker/checker/paper/PhoenixPaperChecker.java
+++ src/main/java/indi/wenyan/content/checker/checker/paper/PhoenixPaperChecker.java
@@ -4,16 +4,37 @@ import indi.wenyan.content.checker.ValueAnswerChecker;
 import indi.wenyan.judou.utils.function.WenyanValues;
 import net.minecraft.util.RandomSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
-
 public class PhoenixPaperChecker extends ValueAnswerChecker {
+    private int solutionCount = 0;
 
     public PhoenixPaperChecker(RandomSource random) {
         super(random);
     }
 
-    // 判断素数的辅助方法
+    private void solve(int row, int n, int cols, int diag1, int diag2) {
+        if (row == n) {
+            solutionCount++;
+            return;
+        }
+        int availablePositions = ((1 << n) - 1) & (~(cols | diag1 | diag2));
 
+        while (availablePositions != 0) {
+            int position = availablePositions & (-availablePositions);
+            availablePositions &= (availablePositions - 1);
+            solve(row + 1, n, cols | position, (diag1 | position) << 1, (diag2 | position) >> 1);
+        }
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        int n = random.nextInt(7) + 4;
+        setVariable(0, WenyanValues.of(n));
+
+        solutionCount = 0;
+        solve(0, n, 0, 0, 0);
+
+        ans = WenyanValues.of(solutionCount);
+    }
 }
