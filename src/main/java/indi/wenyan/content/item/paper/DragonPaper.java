399b5d8efcac39d6d90d42b0d75c887e6184e4a3
ugnjhjf <rokidna@outlook.com>
2026-06-06 16:33:47 +0800

    feat: add easter egg on final fuzhou's ink

--- src/main/java/indi/wenyan/content/item/paper/DragonPaper.java
+++ src/main/java/indi/wenyan/content/item/paper/DragonPaper.java
@@ -1,11 +1,14 @@
 package indi.wenyan.content.item.paper;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.JukeboxSongs;
 
 public class DragonPaper extends Item {
     public static final String ID =  "dragon_paper";
 
     public DragonPaper(Item.Properties properties) {
-        super(properties);
+        super(properties
+                .jukeboxPlayable(JukeboxSongs.LAVA_CHICKEN)
+        );
     }
 }
